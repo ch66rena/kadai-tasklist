48faca4ca4ca0722a97e345afac64ee827dedc4b
eri <techacademyeri@gmail.com>
2018-07-25 12:15:07 +0000

    set preparation

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_16_135118) do
+ActiveRecord::Schema.define(version: 2018_07_23_141854) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
