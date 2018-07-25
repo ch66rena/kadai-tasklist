2fafb3e9692012512dd4ed4befa66148bd73eb67
eri <techacademyeri@gmail.com>
2018-07-25 12:40:32 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
 Rails.application.routes.draw do
-  get 'task/index'
-  get 'task/show'
-  get 'task/new'
-  get 'task/create'
-  get 'toppages/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
   root to: 'toppages#index'
-  
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-
-  get 'signup', to: 'tasks#new'
-  resources :tasks, only: [:index, :show, :new, :create]
 end
