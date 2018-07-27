94c8b1bde30b9b7dec70f1581c29925bea059dda
eri <techacademyeri@gmail.com>
2018-07-27 14:18:34 +0000

    lesson12 ログイン認証と一対多

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,19 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
+  def index
+    @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
+  end
+  
+  def show
+  end
+  
+  def new
+    @task = Task.new
+  end
+
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
@@ -12,6 +26,19 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+  end
+  
+  def update
+    if @task.update(task_params)
+      flash[:success] = 'タスク は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスク は更新されませんでした'
+      render :edit
+    end    
+  end
+ 
   def destroy
     @task.destroy
     flash[:success] = 'タスクを削除しました。'
@@ -29,5 +56,10 @@ class TasksController < ApplicationController
     unless @task
       redirect_to root_url
     end
-  end  
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
 end
